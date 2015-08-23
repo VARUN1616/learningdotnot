1346433c57d0931a931aa6954ad6138ffabf6d85
VARUN1616 <varunbhatia1616@gmail.com>
2015-08-23 15:14:19 +0530

    Using Switch and Enumeration

---
+++ UnderstandingScope/UnderstandingScope/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnderstandingScope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.DoSomething();
+            Console.ReadLine();
+        }
+    }
+    class Car
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine(this.helperMethod());
+        }
+        private string helperMethod()
+        {
+            return "Hello World!";
+        }
+    }
+}
---
+++ UsingSwitch/UsingSwitch/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UsingSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type Superhero's name to see his nick name");
+            string userValue = Console.ReadLine();
+
+            switch (userValue.ToUpper())
+            {
+                case "BATMAN":
+                    Console.WriteLine("safdhngkanf;la");
+                    break;
+                case "SUPERMAN":
+                    Console.WriteLine("kbksabdfvkjdbasf");
+                    break;
+                case "GREENLANTERN":
+                    Console.WriteLine("ndsakfbkajsdfjas");
+                    break;
+
+                default:
+                    Console.WriteLine("Bevkoof");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
